3c8495bfa4f29011abd8794a75458565995d2566
Juan Fernando Cepeda <juanfer2k@gmail.com>
2026-07-13 08:00:09 -0500

    Fix fix22

--- www/header.php
+++ www/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<span class="icon">🌙</span> <span class="label">Noche</span>
+<span class="icon">🌙</span> <span class="label" id="themeLabel">Día</span>
     <base href="/MUNAY/www/">
     <!-- Cargar ambos archivos CSS -->
     <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
